248b10762c216b5f8526a72b714c3384c98bd535
ayushmanZ <108690200+ayushmanZ@users.noreply.github.com>
2022-07-04 22:59:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./Mycomponents/Header";
+import {Mid} from "./Mycomponents/Mid";
+import {Footer} from "./Mycomponents/Footer";
+function App() {
+  return (
+    <>
+      <Header/>
+      <Mid/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Mycomponents/Mid.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Mid = () => {
+  return (
+    <div>middle works!!</div>
+  )
+}
