d19f45b173e6976496431ab9d49073a3b156b86a
Manash Pratim Pathak <mppoff021@gmail.com>
2022-07-09 12:10:23 +0530

    minor change (our team)

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className=" footer bg-dark text-light py-3" style={{position: "absolute", bottom: "0", width: "100%",height: "8vh"}}>
+    <div className=" footer bg-dark text-light py-3" style={{position: "absolute", bottom: "0", width: "100%",height: "8vh", zIndex: "100"}}>
       <p className="text-center"  style={{fontSize: "1rem"}}>
         Copyright &copy; FWSassam.com 
       </p>
