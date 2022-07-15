3f301feb376109b272cd69fb9662098621105c13
Manash Pratim Pathak <mppoff021@gmail.com>
2022-07-15 22:46:21 +0530

    added legends on the second and background on the mission page

--- src/components/OurMission/index.js
+++ src/components/OurMission/index.js
@@ -1,34 +1,44 @@
 import React from "react";
 import "./ourMission.css";
+import bgmo from "../../assests/images/Unflood.jpg"
+
+const style = {
+  backgroundImage: "url(${bgmo})",
+  backgroundSize: '105.9vw',
+        backgroundRepeat: 'no-repeat'
+}
 
 function OurMission() {
   return (
-    <div className="ourMission-div">
-      <h1>THE PRIME VISION</h1>
-      <br />
-      <h4>
-        “I want to be alive,to build a society, I have a promise to build a
-        society Where the value of life is more than that of gold”
+    <div className="bg_img_om" style={{style}}>
+      <div className="ourMission-div">
+        <h1>THE PRIME VISION</h1>
         <br />
-        Dr. Bhupen Hazarika
-      </h4>
-      <br />
-      <hr/>
-      <p>
-        The Rural areas remain the most AFFECTED and most NEGLECTED.
-        <br /><br />
-        <ul>
+        <h4>
+          “I want to be alive,to build a society, I have a promise to build a
+          society Where the value of life is more than that of gold”
+          <br />
+          Dr. Bhupen Hazarika
+        </h4>
+        <br />
+        <hr />
+        <p>
+          The Rural areas remain the most AFFECTED and most NEGLECTED.
+          <br /><br />
+          <ul>
             <li>More than 173 people dead, 29.7 lakh + affected ( Source: Mint, july
-        2022)</li>
+              2022)</li>
             <li>Displaced from their homes and loss of valuables.</li>
             <li>The main motive is to make floods predictable and safer areas
-        identifiable.</li>
-        </ul>
+              identifiable.</li>
+          </ul>
 
-        The Early flood prediction system which will use the power of machine
-        learning and technology and to achieve the motive.
-      </p>
+          The Early flood prediction system which will use the power of machine
+          learning and technology and to achieve the motive.
+        </p>
+      </div>
     </div>
+
   );
 }
 
